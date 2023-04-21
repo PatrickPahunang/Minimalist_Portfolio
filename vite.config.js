dd06a3f9a366878e6aba02d2657041307a550fa5
PatrickPahunang <patrickcd452@gmail.com>
2023-04-22 07:51:56 +0800

    test

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/Minimalist_Portfolio/',
+  base:'/Minimalist_Portfolio/mp',
   plugins: [vue()],
 })
